28e5b0086ac493337f77f7fa50320a8f8c15de64
yebyyy <yexiaomeng2004@gmail.com>
2024-10-31 14:49:00 -0400

    fix django model

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+AUTH_USER_MODEL = 'ta_app.User'
 # Application definition
 
 INSTALLED_APPS = [
--- backend/ta_app/models.py
+++ backend/ta_app/models.py
@@ -1,9 +1,20 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
 class User(AbstractUser):
     is_student = models.BooleanField(default=False)
     is_ta = models.BooleanField(default=False)
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name='custom_user_set',  # Add a custom related_name to avoid conflict
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='custom_user_permissions_set',  # Add a custom related_name to avoid conflict
+        blank=True
+    )
 
 class School(models.Model):
     name = models.CharField(max_length=255)
@@ -12,12 +23,12 @@ class School(models.Model):
         return self.name
 
 class TA(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE) # One to One field is used to create a one-to-one relationship between two models
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     school = models.ForeignKey(School, on_delete=models.CASCADE)
     course = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.user.username # Return the username of the user
+        return self.user.username
     
 class Review(models.Model):
     ta = models.ForeignKey(TA, on_delete=models.CASCADE)
