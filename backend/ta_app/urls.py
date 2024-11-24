d3e5eef6676b5d7608d407960f00e1f1ea8f9cc2
yebyyy <yexiaomeng2004@gmail.com>
2024-11-24 17:21:25 -0500

    backend done with url

---
+++ backend/ta_app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter 
+# router is a way to automatically determine the URL conf for a set of views
+# DefaultRouter is a simple router which creates a set of default routes for the views
+from .views import SchoolViewSet, TAViewSet, ReviewViewSet
+
+router = DefaultRouter()
+#.register() method is used to register a viewset with the router
+router.register(r'schools', SchoolViewSet)  # /schools is registered with SchoolViewSet
+router.register(r'tas', TAViewSet) # /tas is registered with TAViewSet
+router.register(r'reviews', ReviewViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)) # include all the urls from the router
+]
--- backend/ta_app/views.py
+++ backend/ta_app/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+# viewsets is a collection of views (functions) that help us to create CRUD views
+from .models import School, TA, Review, User
+from .serializer import SchoolSerializer, TASerializer, ReviewSerializer, UserSerializer
 
-# Create your views here.
+
+# class based views
+# queryset defines what objects to get from the database
+# serializer_class defines what serializer to use to serialize the objects
+class SchoolViewSet(viewsets.ModelViewSet):
+    queryset = School.objects.all()
+    serializer_class = SchoolSerializer
+
+class TAViewSet(viewsets.ModelViewSet):
+    queryset = TA.objects.all()
+    serializer_class = TASerializer
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
